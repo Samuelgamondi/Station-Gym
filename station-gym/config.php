99540697500a415ee68fdfa55015f2bdd1f44ae4
Samuelgamondi <72472899+Samuelgamondi@users.noreply.github.com>
2025-10-22 00:13:17 -0300

    Add files via upload

---
+++ station-gym/config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'station_gym_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('BASE_URL', 'http://localhost/station-gym/public/');  // ← URL COMPLETA
+define('UPLOAD_DIR', __DIR__ . '/public/img/');
+define('UPLOAD_URL', BASE_URL . '/public/img/');  
+?>
